badcc7208b9e4934d9e01757013e6e56abbe78ba
jeongyunjae <yunjae2295@naver.com>
2022-04-27 10:08:49 +0900

    Updates

---
+++ component---src-pages-404-js-61609c12153d6e5c04fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyunjaaae_com=self.webpackChunkyunjaaae_com||[]).push([[883],{4393:function(e,t,n){n.r(t);var u=n(7294),a=n(5970),l=n(579);t.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-61609c12153d6e5c04fc.js.map
